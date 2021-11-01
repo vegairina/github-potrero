77b2ad4dce29f3874e37148dd463f36014473c35
vegairina <89654897+vegairina@users.noreply.github.com>
2021-11-01 14:27:13 -0300

    Add files via upload

---
+++ js/boton.js
@@ -0,0 +1,62 @@
+function aviso() {alert ("¿Esta dispuesto a ver el mejor sitio del mundo mundial?");}
+function cartelito() {alert ("Esta es una pagina realizada por Irina Vega");
+
+}
+function SitioInclusivo() {document.body.style.backgroundColor = "black";
+
+ document.getElementById("luis").style.fontFamily = "sans-serif";
+document.getElementById("luis").style.fontSize = "2rem";
+document.getElementById("luis").style.padding = " 10px 10px 20px 10px";
+document.getElementById("h1tacos").style.fontSize = "5rem";
+document.getElementById("h1tacos").style.backgroundColor  = "white";
+document.getElementById("nav").style.backgroundColor  = "black";
+document.getElementById("footer").style.backgroundColor = "black";
+document.getElementById("donde").style.backgroundColor = "white";
+document.getElementById("donde").style.fontSize = "5rem";
+document.getElementById("SI").style.display = "none";
+
+document.getElementById("header").style.height = "400px";
+document.getElementById("header").style.backgroundSize = "1200px 400px";
+document.getElementById("h1header").style.marginTop = "200px";
+
+document.getElementById("tacografia").style.display = "none";
+document.getElementById("lateral2").style.display = "none";
+
+document.getElementById("main").style.flexFlow = "column nowrap";
+document.getElementById("section1").style.width = "100%";
+document.getElementById("section1").style.height = "550px";
+document.getElementById("lateral").style.width = "100%";
+document.getElementById("lateral3").style.width = "100%";
+
+document.getElementById("contacto").style.width = "600px";
+document.getElementById("contacto").style.marginLeft= "300px";
+document.getElementById("contacto").style.fontSize = "1.8rem";
+document.getElementById("btnp").style.fontSize = "1.8rem";
+document.getElementById("btnc").style.fontSize = "1.8rem";
+document.getElementById("btnvg").style.fontSize = "1.8rem";
+
+document.getElementById("section1").style.backgroundColor = "white";
+document.getElementById("lateral").style.backgroundColor = "black";
+document.getElementById("lateral3").style.backgroundColor = "white";
+
+document.getElementById("lateral2").style.marginRight = "100px";
+document.getElementById("lateral").style.marginTop = "600px";
+document.getElementById("lateral3").style.marginTop = "50px";
+
+document.getElementById("mapa").style.width = "90%";
+document.getElementById("mapa").style.marginLeft = "50px";
+
+
+}
+function cambiarfondo2() {
+  document.body.style.backgroundColor = "blue";
+
+}
+
+
+
+
+/*Un Botón que al presionarlo convierta nuestra
+pagina en un sitio inclusivo apto para personas con
+discapacidad visual (fondos oscuros y letras claras-
+   ejemplo https://golinc.com.ar).*/
